25bf8fa388ee838eedbe4e236eca057b3da75fbf
Jonathan Hauwaerts <jojo_hauwaerts@hotmail.com>
2020-07-28 15:44:37 +0200

    Changed file structure

---
+++ angular 8/back-end/Listapp/Models/ListappDatabaseSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Listapp.Models
+{
+    public class ListappDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string UsersCollectionName { get; set; }
+    }
+
+    public interface IListappDatabaseSettings
+    {
+            string UsersCollectionName { get; set; }
+            string ConnectionString { get; set; }
+            string DatabaseName { get; set; }
+        
+    }
+}
